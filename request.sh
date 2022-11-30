c628bcc634cabf5a4a66241499dab158106d6088
yuharsenergi <yuharsenergi@gmail.com>
2022-12-01 02:42:54 +0700

    Update request.sh example

--- request.sh
+++ request.sh
@@ -3,14 +3,21 @@
 max=$1
 counter=1
 s=$2
-if [[ s -eq 0 ]]
-then s=0.5
-fi
+
 while [ $counter -le $max ]; do
-#while true; do
 	echo "Try $counter"
-	curl -X POST --connect-timeout 2 http://localhost:9000/uuid -H 'Content-Type: application/json' -d '{"message":"hello"}'
+jsonString="{\
+\"time\":\"`date`\",\
+\"uuid\":\"`uuidgen`\"\
+}"
+	echo "[>>] $jsonString"
+	printf "[<<] " && curl -X POST \
+		--connect-timeout 2 \
+		http://localhost:9000/uuid \
+		-H 'Content-Type: application/json' \
+		-d "$jsonString";
+	printf "\n"
 	counter=$(( $counter + 1 ))
-	echo ""
-	sleep .$s
+#	echo ""
+	if [[ "$s" -gt 0 ]]; then sleep $s; fi
 done
