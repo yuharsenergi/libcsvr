f97b58a850e6e6ff87afbf0d0208500038a02231
yuharsen.ergi <yuharsen.ergi@1rstwap.com>
2022-11-30 14:54:14 +0700

    Update valgrind command

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-valgrind --leak-check=full --track-origins=yes --log-file=$1-valgrind-check.txt $1
+if [ "$#" -ne 1 ]; then
+    printf "\n-- Binary file is mandatory! --\n\n"
+    echo "Example :"
+    printf "    $0 ./your/binary/file\n\n"
+    exit 0
+fi
+
+fileType=$(file -b --mime-type $1 | sed 's|/.*||')
+if [ "$fileType" != "application"  ]; then
+    printf "\n-- '$1' is not a binary file! --\n\n"
+    exit 0
+fi
+
+mkdir -p valgrind
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind/$1-valgrind-check.txt $1
