a058ce342ef208b36e69d1154f91285ddd24fc87
yuharsenergi <yuharsenergi@gmail.com>
2022-12-11 22:51:54 +0700

    add initial tls example

---
+++ example/syncronous_TLS/src/threads.c
@@ -0,0 +1,106 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <pthread.h>
+
+#include "libcsvr.h"
+#include "libcsvr_tls.h"
+#include "threads.h"
+
+static pthread_t thrServer;
+bool isRunning = false;
+
+void shutdownThreads(void);
+void joinThreads(void);
+void *threadServer(void *);
+
+typedef struct
+{
+    csvrTlsRequest_t *request;
+}cbHandler_t;
+
+void threadsCallback(void *arg)
+{
+    cbHandler_t* data = (cbHandler_t*)arg;
+    if(data)
+    {
+        if(data->request){
+            free(data->request);
+        }
+        free(data);
+    }
+    printf("Server threads is dead!\n");
+    isRunning = false;
+}
+
+void *threadServer(void *arg)
+{
+    csvrTlsServer_t *server = (csvrTlsServer_t*)arg;
+    isRunning = true;
+    cbHandler_t* data = NULL;
+    data = calloc(1, sizeof(cbHandler_t));
+    if(data == NULL)
+    {
+        pthread_exit(NULL);
+    }
+    pthread_cleanup_push(threadsCallback, (void*)data);
+    if(pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL) != 0)
+    {
+        pthread_exit(NULL);
+    }
+
+    // Initialize the SSL library
+    SSL_library_init();
+
+    while(1)
+    {
+        csvrTlsRequest_t * request = NULL;
+        request = calloc(1,sizeof(csvrTlsRequest_t));
+        if(request == NULL) break;
+
+        memset(request,0, sizeof(csvrTlsRequest_t));
+        csvrTlsRead(server, request);
+        printf("[ <<< ] %s\n",request->content);
+        char *response = "{\"status\":\"OK\"}";
+        csvrTlsSend(request, response, strlen(response));
+        printf("[ >>> ] %s\n",response);
+        csvrTlsReadFinish(request);
+        free(request);
+    }
+    pthread_cleanup_pop(0);
+    pthread_exit(NULL);
+}
+
+int initThreads(csvrTlsServer_t *server)
+{
+    int ret = -1;
+    if(!csvrCheckRoot())
+    {
+        return -1;
+    }
+    ret = pthread_create(&thrServer,NULL,threadServer,(void*)server);
+    if(ret != 0)
+    {
+        printf("Failed init server threads!\n");
+    }
+
+    return ret;
+}
+
+void joinThreads(void)
+{
+    pthread_join(thrServer,NULL);
+}
+
+void shutdownThreads(void)
+{
+    if(isRunning)
+    {
+        pthread_cancel(thrServer);
+        pthread_join(thrServer,NULL);
+    }
+}
